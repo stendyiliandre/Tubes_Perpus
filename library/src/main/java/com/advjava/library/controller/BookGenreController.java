b344b316117396ade023c154a90fdf1a3ffa5850
Stendy Iliandre <33411219+stendyiliandre@users.noreply.github.com>
2020-04-25 22:57:39 +0700

    BookGenre

--- library/src/main/java/com/advjava/library/controller/BookGenreController.java
+++ library/src/main/java/com/advjava/library/controller/BookGenreController.java
@@ -5,14 +5,20 @@ import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.advjava.library.model.Book;
 import com.advjava.library.model.BookGenre;
+import com.advjava.library.model.BorrowedBy;
 import com.advjava.library.model.Genre;
+import com.advjava.library.model.Member;
 import com.advjava.library.repository.BookRepository;
 import com.advjava.library.repository.BookGenreRepository;
 import com.advjava.library.repository.GenreRepository;
@@ -31,15 +37,42 @@ public class BookGenreController {
 	public @ResponseBody ResponseEntity<BookGenre> addNewBookGenre (@RequestParam int book_id, @RequestParam int genre_id) {
 	    try {
 	    	BookGenre bookGenreData = new BookGenre();
-//	    	Book book = bookRepository.findById(book_id).get();
-//	    	Genre genre = genreRepository.findById(genre_id).get();
+	    	Book book = bookRepository.findById(book_id).get();
+	    	Genre genre = genreRepository.findById(genre_id).get();
 	    	
-//	    	bookGenreData.setBook_id(book_id);
-//	    	bookGenreData.setGenre_id(genre_id);
-//	    	bookGenreData.setBook(book);
-//	    	bookGenreData.setGenre(genre);
+	    	bookGenreData.setBook(book);
+	    	bookGenreData.setGenre(genre);
 	    	bookGenreRepository.save(bookGenreData);
 	        return ResponseEntity.ok(bookGenreData); 
+		}catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@GetMapping(path="/BookGenre")
+	public @ResponseBody ResponseEntity<Iterable<BookGenre>> getAllBookGenres() {
+		try {
+			return ResponseEntity.ok(bookGenreRepository.findAll()); 
+		}catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@GetMapping(path="/BookGenre/{id}")
+	public @ResponseBody ResponseEntity<BookGenre> getBookGenres(@PathVariable int id) {
+		try {
+			BookGenre bookGenreData = bookGenreRepository.findById(id).get();
+			return ResponseEntity.ok(bookGenreData); 
+		}catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping(path="/BookGenre/{id}")
+	public @ResponseBody ResponseEntity<Void> deleteBookGenre(@PathVariable int id){
+		try {
+			bookGenreRepository.deleteById(id);
+			return ResponseEntity.ok().build();
 		}catch (Exception e) {
 			return ResponseEntity.notFound().build();
 		}
--- library/src/main/java/com/advjava/library/model/BookGenre.java
+++ library/src/main/java/com/advjava/library/model/BookGenre.java
@@ -33,4 +33,30 @@ public class BookGenre {
 	@JoinColumn(name = "genre_id", referencedColumnName = "id")
 	private Genre genre;
 
+	public BookGenrePK getBookGenrePK() {
+		return bookGenrePK;
+	}
+
+	public void setBookGenrePK(BookGenrePK bookGenrePK) {
+		this.bookGenrePK = bookGenrePK;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
+	
+
 }
