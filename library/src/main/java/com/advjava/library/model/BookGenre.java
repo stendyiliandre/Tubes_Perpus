44322605a103c03dd878724082b194a27f6e60e9
Stendy Iliandre <33411219+stendyiliandre@users.noreply.github.com>
2020-04-25 22:24:41 +0700

    Update BookGenre

--- library/src/main/java/com/advjava/library/model/BookGenre.java
+++ library/src/main/java/com/advjava/library/model/BookGenre.java
@@ -1,68 +1,36 @@
 package com.advjava.library.model;
 
 import java.io.Serializable;
-import java.sql.Date;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.IdClass;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 
 @Entity
-@IdClass(BookGenre.class)
-public class BookGenre implements Serializable {
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-//	private int genre_id;
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-//	private int book_id;
+public class BookGenre {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-    @PrimaryKeyJoinColumn(name = "book_id", referencedColumnName = "id")
-    private Book book;
+	@Embeddable
+	static class BookGenrePK implements Serializable {
+	   private int book_id;
+	   private int genre_id;
+	   private static final long serialVersionUID = 1L;
+	}
 	
-	@ManyToOne(fetch = FetchType.LAZY)
-    @PrimaryKeyJoinColumn(name = "genre_id", referencedColumnName = "id")
-    private Genre genre;
+	@EmbeddedId
+	private BookGenrePK bookGenrePK;
+	
+	@MapsId("book_id")
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "book_id", referencedColumnName = "id")
+	private Book book;
 
-//	public int getGenre_id() {
-//		return genre_id;
-//	}
-//
-//	public void setGenre_id(int genre_id) {
-//		this.genre_id = genre_id;
-//	}
+	@MapsId("genre_id")
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "genre_id", referencedColumnName = "id")
+	private Genre genre;
 
-//	public int getBook_id() {
-//		return book_id;
-//	}
-//
-//	public void setBook_id(int book_id) {
-//		this.book_id = book_id;
-//	}
-//
-//	public Book getBook() {
-//		return book;
-//	}
-//
-//	public void setBook(Book book) {
-//		this.book = book;
-//	}
-//
-//	public Genre getGenre() {
-//		return genre;
-//	}
-//
-//	public void setGenre(Genre genre) {
-//		this.genre = genre;
-//	}
 }
